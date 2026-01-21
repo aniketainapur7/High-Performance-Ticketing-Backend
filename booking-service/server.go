fac2bae4b5044a2d8fabbe610f936f66196f8ccc
AniketAinapur <aniketainapur2@gmail.com>
2026-01-21 19:25:35 +0530

    Added postgres with go

--- booking-service/server.go
+++ booking-service/server.go
@@ -8,6 +8,7 @@ import (
 
 	"booking-service/bookingpb"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
 )
@@ -15,6 +16,7 @@ import (
 type server struct {
 	bookingpb.UnimplementedBookingServiceServer
 	redis *redis.Client
+	db    *pgx.Conn
 }
 
 func (s *server) ConfirmBooking(
@@ -24,11 +26,12 @@ func (s *server) ConfirmBooking(
 
 	seatId := req.SeatId
 	userId := req.UserId
+	trainId := req.TrainId
 
 	lockKey := fmt.Sprintf("seat:%d:lock", seatId)
 	lockedBy, err := s.redis.Get(ctx, lockKey).Result()
 
-	bitmapKey := fmt.Sprintf("train:%d:seats",req.TrainId)
+	bitmapKey := fmt.Sprintf("train:%d:seats", trainId)
 	seatIndex := int64(seatId - 1)
 
 	if err == redis.Nil {
@@ -48,11 +51,11 @@ func (s *server) ConfirmBooking(
 	if lockedBy != userId {
 		return &bookingpb.ConfirmBookingResponse{
 			Success: false,
-			Error: "Locked By Other User",
-		},nil
+			Error:   "Locked By Other User",
+		}, nil
 	}
 
-	booked,err := s.redis.GetBit(ctx,bitmapKey,seatIndex).Result()
+	booked, err := s.redis.GetBit(ctx, bitmapKey, seatIndex).Result()
 
 	if err != nil {
 		return &bookingpb.ConfirmBookingResponse{
@@ -64,25 +67,37 @@ func (s *server) ConfirmBooking(
 	if booked == 1 {
 		return &bookingpb.ConfirmBookingResponse{
 			Success: false,
-			Error: "Seat Already Booked",
-		},nil
+			Error:   "Seat Already Booked",
+		}, nil
 	}
-	_,err = s.redis.SetBit(ctx,bitmapKey,seatIndex,1).Result()
+	_, err = s.redis.SetBit(ctx, bitmapKey, seatIndex, 1).Result()
 
 	if err != nil {
-    	return &bookingpb.ConfirmBookingResponse{
-    	Success: false,
-    	Error:   "REDIS_ERROR",
-    }, nil
-  	}
+		return &bookingpb.ConfirmBookingResponse{
+			Success: false,
+			Error:   "REDIS_ERROR",
+		}, nil
+	}
+
+	_, err = s.db.Exec(ctx, `INSERT INTO bookings(id,seat_id,train_id,user_id,status)
+						  VALUES(gen_random_uuid(),$1,$2,$3,"CONFIRMED")`,
+						seatId, trainId, userId);
+	
+	if err != nil {
+		s.redis.SetBit(ctx,bitmapKey,seatIndex,0)
+		return &bookingpb.ConfirmBookingResponse{
+			Success: false,
+			Error: "DB_ERROR",
+		},nil
+	}
+
+	s.redis.Del(ctx,lockKey)
 
 
 	return &bookingpb.ConfirmBookingResponse{
-		Success:   true,
+		Success: true,
 	}, nil
 }
-
-
 
 func main() {
 	rdb := redis.NewClient(&redis.Options{
@@ -99,6 +114,17 @@ func main() {
 
 	log.Println("Redis Connection Successful !!")
 
+	dbConn, err := pgx.Connect(
+		context.Background(),
+		"postgresql://ticket:ticket@localhost:5432/ticketing",
+	)
+
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+	}
+
+	log.Println("Connected to Postgres")
+
 	lis, err := net.Listen("tcp", ":50051")
 
 	if err != nil {
@@ -109,7 +135,10 @@ func main() {
 
 	bookingpb.RegisterBookingServiceServer(
 		grpcServer,
-		&server{redis: rdb},
+		&server{
+			redis: rdb,
+			db:    dbConn,
+		},
 	)
 
 	log.Println("GRPC SERVICE RUNNING ON PORT 50051")
